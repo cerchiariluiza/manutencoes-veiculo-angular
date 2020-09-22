e49ff6c23382853b34d74f85d3624c6e682eee9c
cerchiariluiza <luiza.data.developer@gmail.com>
2020-09-22 20:29:00 -0300

    getall and getone

--- frontend/controller.js
+++ frontend/controller.js
@@ -4,14 +4,39 @@ app.controller('myCtrl', function($scope, $http) {
   $scope.dia = "21/09/2020";
   $scope.valor = "19.90";
   $scope.nome = "Luiza";
-  $scope.salvar = function(){
-    alert($scope.dia);
-}
-
-$http.get("http://localhost:8080/manutencoes")
-//  https://chrome.google.com/webstore/detail/moesif-origin-cors-change/digfbfaphojjndkpccljibejjbppifbc?hl=pt-BR  .then(function(response) {
-
-    .then(function(response) {
-      console.info(response)
+  
+  $scope.save = function(){
+    $http.post("http://localhost:8080/manutencoes")
+    .then(function(resposta) {
+        console.info(resposta);
     });
+  }
+
+  $scope.getAll = function(){
+    $http.get("http://localhost:8080/manutencoes")
+    .then(function(resposta) {
+        console.info(resposta);
+    });
+
+  }
+  $scope.getOne = function(){
+    $http.get("http://localhost:8080/manutencoes/1")
+    .then(function(resposta) {
+        console.info(resposta);
+    });
+  }
+
+  $scope.delete = function(){
+    $http.delete("http://localhost:8080/manutencoes/1")
+    .then(function(resposta) {
+        console.info(resposta);
+    });
+  }
+  $scope.put = function(){
+    $http.put("http://localhost:8080/manutencoes/1")
+    .then(function(resposta) {
+        console.info(resposta);
+    });
+  }
+
   });
