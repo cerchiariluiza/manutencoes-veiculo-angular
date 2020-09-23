8d2fdf4217987b55e02d2c795ec66b49e5a96f08
cerchiariluiza <luiza.data.developer@gmail.com>
2020-09-22 21:45:27 -0300

    crud completo

--- frontend/controller.js
+++ frontend/controller.js
@@ -1,13 +1,21 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
-  $scope.dia = "21/09/2020";
+  $scope.dia = "2020-09-23T00:35:45.854+00:00";
   $scope.nome = "Luiza";
   $scope.valor = "19.90";
 
   
   $scope.insert = function(){
     $http.post("http://localhost:8080/manutencoes",{
+      "valor": $scope.valor,
+      "dia":  $scope.dia,
+      "nome": $scope.nome
+    })
+  }
+
+  $scope.put = function(){
+    $http.put("http://localhost:8080/manutencoes/2",{
       "valor": $scope.valor,
       "dia":  $scope.dia,
       "nome": $scope.nome
@@ -34,11 +42,6 @@ app.controller('myCtrl', function($scope, $http) {
         console.info(resposta);
     });
   }
-  $scope.put = function(){
-    $http.put("http://localhost:8080/manutencoes/1")
-    .then(function(resposta) {
-        console.info(resposta);
-    });
-  }
+ 
 
   });
